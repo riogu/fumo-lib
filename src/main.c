3750bcaa198bf867915c27d4d21ad8874c490b81
riogu <egas.g.ribeiro@gmail.com>
2025-05-18 23:09:20 +0100

    ee

--- src/main.c
+++ src/main.c
@@ -4,74 +4,74 @@
 
 // #define switch auto
 
-int main() {
-    Rectangle rect = {.width = 123, .height = 1231};
-    struct fumo$variant variant = fumo$variant(rect); // making variant struct
-
-    fumo$match(variant) {
-        fumo$case(Shape) {
-            _Shape->shape_id = 123;
-        }
-        fumo$case(Rectangle) {
-            printf("old_height: %d\n", _Rectangle->height);
-            _Rectangle->height = 2131;
-            printf("new_height: %d\n", _Rectangle->height);
-        }
-        fumo$default {
-            printf("we dont get a value here");
-        }
-    }
-
-    return 0;
-}
-
 // int main() {
+//     Rectangle rect = {.width = 123, .height = 1231};
+//     struct fumo$variant variant = fumo$variant(rect); // making variant struct
 //
-//     rectangle rect = {.width = 123, .height = 1231};
-//     fumo$variant variant = fumo$variant(rect);
-//
-// fumo$is_same_t((Position) {}, variant) ? printf("true\n") : printf("false\n");
-// // false
-// fumo$is_same_t(rect, variant) ? printf("true\n") : printf("false\n");
-// // true
-// fumo$is_same_t((Shape) {}, rect) ? printf("true\n") : printf("false\n");
-// false
-//
-//     auto result1 = fumo$get_if(Rectangle, variant) {
-//         printf("width before: %d \n", result1->width);
-//
-//         result1->width = 213123;
-//         printf("there was a rectangle, width: %d\ntype_name: %s\n\n",
-//                result1->width,
-//                fumo$variant_type_name(variant));
+//     fumo$match(variant) {
+//         fumo$case(Shape) {
+//             _Shape->shape_id = 123;
+//         }
+//         fumo$case(Rectangle) {
+//             printf("old_height: %d\n", _Rectangle->height);
+//             _Rectangle->height = 2131;
+//             printf("new_height: %d\n", _Rectangle->height);
+//         }
+//         fumo$default {
+//             printf("we dont get a value here");
+//         }
 //     }
-//     else {
-//         printf("didnt have a rectangle!\n");
-//     }
-//
-//     Position pos = {.x = 69420};
-//     variant = fumo$variant(pos);
-//
-//     auto result2 = fumo$get_if(Shape, variant) {
-//         // is never reached
-//         result2->shape_id = 213;
-//     }
-//     else {
-//         // fails to get the value, result2 is NULL
-//         printf("couldn't get shape from variant.\n");
-//         printf("type in variant: %s\n", fumo$variant_type_name(variant));
-//     }
-//
-//     // does nothing
-//     auto shape = fumo$get_if(Shape, variant) {
-//         printf("my shape id: %d\n", shape->shape_id);
-//         // you can write the code assuming you have the right type
-//         // but its only ran if the stored type matches Shape
-//
-//     } // the else{} is optional
 //
 //     return 0;
 // }
+
+int main() {
+
+    Rectangle rect = {.width = 123, .height = 1231};
+    fumo$variant variant = fumo$variant(rect);
+
+    fumo$is_same_t((Position) {}, variant) ? printf("true\n") : printf("false\n");
+    // false
+    fumo$is_same_t(rect, variant) ? printf("true\n") : printf("false\n");
+    // true
+    fumo$is_same_t((Shape) {}, rect) ? printf("true\n") : printf("false\n");
+    // false
+
+    auto result1 = fumo$get_if(Rectangle, variant) {
+        printf("width before: %d \n", result1->width);
+
+        result1->width = 213123;
+        printf("there was a rectangle, width: %d\ntype_name: %s\n\n",
+               result1->width,
+               fumo$variant_type_name(variant));
+    }
+    else {
+        printf("didnt have a rectangle!\n");
+    }
+
+    Position pos = {.x = 69420};
+    variant = fumo$variant(pos);
+
+    auto result2 = fumo$get_if(Shape, variant) {
+        // is never reached
+        result2->shape_id = 213;
+    }
+    else {
+        // fails to get the value, result2 is NULL
+        printf("couldn't get shape from variant.\n");
+        printf("type in variant: %s\n", fumo$variant_type_name(variant));
+    }
+
+    // does nothing
+    auto shape = fumo$get_if(Shape, variant) {
+        printf("my shape id: %d\n", shape->shape_id);
+        // you can write the code assuming you have the right type
+        // but its only ran if the stored type matches Shape
+
+    } // the else{} is optional
+
+    return 0;
+}
 
 //
 // *(volatile char*)0 = 0;
