fda846d624b38da546fea3775f4d4b15c57f8345
riogu <egas.g.ribeiro@gmail.com>
2025-05-18 17:34:10 +0100

    more examples

--- src/main.c
+++ src/main.c
@@ -14,11 +14,12 @@ int main() {
     // false
 
     auto result1 = fumo$get_if(Rectangle, variant) {
-
         printf("width before: %d \n", result1->width);
+
         result1->width = 213123;
-        printf("there was a rectangle, width: %d\n", result1->width);
-        printf("type_name: %s\n\n", fumo$variant_type_name(variant));
+        printf("there was a rectangle, width: %d\ntype_name: %s\n\n",
+               result1->width,
+               fumo$variant_type_name(variant));
     }
     else {
         printf("didnt have a rectangle!\n");
@@ -36,6 +37,16 @@ int main() {
         printf("couldn't get shape from variant.\n");
         printf("type in variant: %s\n", fumo$variant_type_name(variant));
     }
+
+    // does nothing
+    //
+    auto shape = fumo$get_if(Shape, variant) {
+        printf("my shape id: %d\n", shape->shape_id);
+        // you can write the code assuming you have the right type
+        // but its only ran if the stored type matches Shape
+
+    } // the else{} is optional
+
     return 0;
 }
 
