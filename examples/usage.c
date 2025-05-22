7103bdb34cd64c5b5b57ae98ca1140bfbce91a0c
alplusplus <typeal2010@gmail.com>
2025-05-22 11:52:58 +0900

    update basic usage

--- examples/usage.c
+++ examples/usage.c
@@ -1,63 +1,30 @@
-#include <stdio.h>
-#include "fumo/fumo_data_structures.h"
+#include "fumo/fumo_c_definitions.h"
 #include "fumo/fumo_type_name.h"
 
+// Basic usage
 int main() {
-	// Primitive type
-	int num = 42;
-	float pi = 3.14159f;
-	char* message = "Hello, variants!";
+    int x = 123;
+    PRINTF("this is x: ", x);
 
-	Variant int_var = Variant(num);
-	Variant float_var = Variant(pi);
-	Variant str_var = Variant(message);
+    Rectangle rect = {.width = 123, .height = 1231};
+    Variant var = Variant(rect);
 
-	// Custom type
-	Position pos = {.x = 10, .y = 20};
-	Rectangle rect = {.width = 30, .height = 40};
+    let inner_value = get_if(Shape, var) {
+        inner_value->shape_id = 123123;
+    }
+    else {
+        printf("failed, type: %s\n", type_name(var));
+    }
 
-	Variant pos_var = Variant(pos);
-	Variant rect_var = Variant(rect);
+    match(var) ({
+        case(Shape, var) var->shape_id = 213;
+        case(Rectangle, rect) {
+            rect->height = 123;
+        }
+        _default {
+            printf("unknown type\n");
+        }
+    });
 
-	// Get type info
-	printf("int_var contains: %s\n", ___variant_type_name(int_var));
-	printf("float_var contains: %s\n", ___variant_type_name(float_var));
-	printf("str_var contains: %s\n", ___variant_type_name(str_var));
-	printf("pos_var contains: %s\n", ___variant_type_name(pos_var));
-	printf("rect_var contains: %s\n", ___variant_type_name(rect_var));
-
-	// Accessing and modifying values
-	{
-		match(int_var) {
-			case(int) {
-				printf("value: %d\n", *_int);
-				*_int *= 2;
-				printf("modified value: %d\n", *_int);
-			}
-			_default printf("Not an int\n");
-		}
-	}
-
-	{
-		match(pos_var) {
-			case(Position) {
-				printf("before: (%d, %d)\n", _Position->x, _Position->y);
-				_Position->x += 5;
-				_Position->y += 10;
-				printf("after: (%d, %d)\n", _Position->x, _Position->y);
-			}
-			_default printf("not position\n");
-		}
-	}
-
-	// Type checking
-	if (is_same_t(pos, pos_var)) {
-		printf("pos_var contains a Position\n");
-	}
-
-	if (!is_same_t(rect, pos_var)) {
-		printf("pos_var does not contain a Rectangle\n");
-	}
-
-	return 0;
+    return 0;
 }
