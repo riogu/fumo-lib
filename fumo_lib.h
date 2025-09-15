3f87992adc5e2b99a515cb01e92f98e860fc4eb4
riogu <egas.g.ribeiro@gmail.com>
2025-09-15 01:02:11 +0100

    moved examples to example_structs.h file

--- fumo_lib.h
+++ fumo_lib.h
@@ -1,24 +1,9 @@
 #pragma once
 
-// clang-format off
-// ----------------------------------------------------------------
-// example structs for testing, unnecessary for usage
-// structs should be included before the macros.
-typedef struct Position {int x;int y;} Position;
-typedef struct Shape {int shape_id;} Shape;
-typedef struct Body {Position position;float radius;} Body;
-typedef struct Rectangle {int width;int height;} Rectangle;
-typedef struct Circle {float radius;} Circle;
-typedef struct Piece {} Piece;
-typedef struct Camera {} Camera;
-typedef struct Board {} Board;
-
-// NOTE: structs should be typedef'd. we want them in the global namespace for this lib.
-// implementation can be changed to allow for using the struct keyword too.
-
+#include "example_structs.h"
 // ----------------------------------------------------------------
 // write your user made structs here in the macro, in this format
-#define user_types Position, Shape, Body, Rectangle
+// #define user_types Position, Shape, Body, Rectangle, etc...
 // ----------------------------------------------------------------
 
 // fumo_lib implementation starts here
