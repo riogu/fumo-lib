d67cf743aca800053f6a93e9dd84605b224baf66
Egas Ribeiro <egas.g.ribeiro@gmail.com>
2026-01-01 22:27:57 +0000

    reorganized header

--- fumo_lib.h
+++ fumo_lib.h
@@ -1,28 +1,3 @@
-// ----------------------------------------------------------------
-// ----------------------------------------------------------------
-// ----------------------------------------------------------------
-// example structs for testing, unnecessary for usage
-// structs should be included before the macros.
-typedef struct Position {int x;int y;} Position;
-typedef struct Shape {int shape_id;} Shape;
-typedef struct Body {Position position;float radius;} Body;
-typedef struct Rectangle {int width;int height;} Rectangle;
-typedef struct Circle {float radius;} Circle;
-typedef struct Piece {} Piece;
-typedef struct Camera {} Camera;
-typedef struct Board {} Board;
-// NOTE: structs should be typedef'd. we want them in the global namespace for this lib.
-// implementation can be changed to allow for using the struct keyword too.
-
-// ----------------------------------------------------------------
-// write your user made structs here in the macro, in this format
-#define user_types Position, Shape, Body, Rectangle
-// ----------------------------------------------------------------
-
-// ----------------------------------------------------------------
-// ----------------------------------------------------------------
-// ----------------------------------------------------------------
-
 #include <stdbool.h>
 #include <wchar.h>
 // map-macro implementation is from:
